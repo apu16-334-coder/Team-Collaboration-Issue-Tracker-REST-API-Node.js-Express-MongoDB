328d654b22ef70114ab90d1036ef75a217c0a188
Muhammad Apu <apu16-334@diu.edu.bd>
2026-04-25 16:25:54 +0600

    Added helmet, core and express-rate-limit middleware for extra secure api

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const helmet = require('helmet')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 
 const { noRouteFound, globalErrorHandler } = require('./middlewares/error.middleware.js')
 
@@ -7,8 +10,20 @@ const app = express();
 // Query parser extended
 app.set('query parser', 'extended');
 
+// Security middlewares
+app.use(helmet())
+app.use(cors())
+
 // Body parser: limit JSON size to 10kb to prevent large payload abuse
 app.use(express.json({ limit: '10kb'}));
+
+// Rate Limiter: limit requests to 100 per IP per hour
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again later."
+})
+app.use('/api', limiter)
 
 // Health route
 app.get('/', (req, res) =>{
@@ -18,7 +33,12 @@ app.get('/', (req, res) =>{
     })
 })
 
+/* ---------- ROUTES ---------- */
+
+
+
 /* ---------- ERROR HANDLERS ---------- */
+
 app.use(noRouteFound)
 
 app.use(globalErrorHandler)
