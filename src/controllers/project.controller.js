3ada6c7570cd2909dff4e21a57d35c0130eb1b67
Muhammad Apu <apu16-334@diu.edu.bd>
2026-05-19 21:07:05 +0600

    Implemented create project route and controller

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -42,172 +42,172 @@ const createProject = catchAsync(
     }
 )
 
-/**
- * GetAllProjects
- * Admin-only: get all the projects
- * GET /api/v1/projects
- */
-const getAllProjects = catchAsync(
-    /** @type {RequestHandler} */
-    async (req, res, next) => {
-        const features = new ApiFeatures(Projects.find(), req.query)
-            .filter()
-            .search('title', 'description')
-            .sort()
-            .pagination()
+// /**
+//  * GetAllProjects
+//  * Admin-only: get all the projects
+//  * GET /api/v1/projects
+//  */
+// const getAllProjects = catchAsync(
+//     /** @type {RequestHandler} */
+//     async (req, res, next) => {
+//         const features = new ApiFeatures(Projects.find(), req.query)
+//             .filter()
+//             .search('title', 'description')
+//             .sort()
+//             .pagination()
 
-        // execute query 
-        const projects = await features.query.populate([
-            { path: 'team', select: 'title' }
-        ]);
+//         // execute query 
+//         const projects = await features.query.populate([
+//             { path: 'team', select: 'title' }
+//         ]);
 
-        // count total without pagination
-        const total = await Projects.countDocuments(features.getQueryObjForCount());
+//         // count total without pagination
+//         const total = await Projects.countDocuments(features.getQueryObjForCount());
 
-        // Send response meta-data for pagination
-        res.status(200).json({
-            success: true,
-            results: projects.length,
-            total,
-            page: features.page,
-            limit: features.limit,
-            data: projects
-        })
-    }
-)
+//         // Send response meta-data for pagination
+//         res.status(200).json({
+//             success: true,
+//             results: projects.length,
+//             total,
+//             page: features.page,
+//             limit: features.limit,
+//             data: projects
+//         })
+//     }
+// )
 
-/**
- * GetTeam
- * (admin | team_lead of team): get a particular team by id
- * GET /api/v1/teams/:id
- */
-const getProject = catchAsync(
-    /** @type {RequestHandler} */
-    async (req, res, next) => {
-        // Find team
-        const team = await Teams.findById(req.params.id)
-            .populate([
-                { path: 'teamLead', select: 'name email' },
-                { path: 'members', select: 'name email' }
-            ]);
+// /**
+//  * GetTeam
+//  * (admin | team_lead of team): get a particular team by id
+//  * GET /api/v1/teams/:id
+//  */
+// const getProject = catchAsync(
+//     /** @type {RequestHandler} */
+//     async (req, res, next) => {
+//         // Find team
+//         const team = await Teams.findById(req.params.id)
+//             .populate([
+//                 { path: 'teamLead', select: 'name email' },
+//                 { path: 'members', select: 'name email' }
+//             ]);
 
-        if (!team) return next(new AppError(404, 'Team is not found'));
+//         if (!team) return next(new AppError(404, 'Team is not found'));
 
-        // if logged user is not admin
-        if (req.user.role !== 'admin') {
-            // then if team is not active
-            if (!team.isActive) return next(new AppError(404, 'Team is not found'));
-        }
+//         // if logged user is not admin
+//         if (req.user.role !== 'admin') {
+//             // then if team is not active
+//             if (!team.isActive) return next(new AppError(404, 'Team is not found'));
+//         }
 
-        // if logged user is not team lead of the team
-        if (req.user.role === 'team_lead' && team.teamLead.id.toString() !== req.user.id) {
-            return next(new AppError(403, 'you can not access'));
-        }
+//         // if logged user is not team lead of the team
+//         if (req.user.role === 'team_lead' && team.teamLead.id.toString() !== req.user.id) {
+//             return next(new AppError(403, 'you can not access'));
+//         }
 
-        // if logged user is not member of the team
-        if (req.user.role === 'member' && !team.members.includes(req.user.id)) {
-            return next(new AppError(403, 'you can not access'));
-        }
+//         // if logged user is not member of the team
+//         if (req.user.role === 'member' && !team.members.includes(req.user.id)) {
+//             return next(new AppError(403, 'you can not access'));
+//         }
 
-        res.status(200).json({
-            success: true,
-            data: team
-        })
-    }
-)
+//         res.status(200).json({
+//             success: true,
+//             data: team
+//         })
+//     }
+// )
 
-/**
- * UpdateTeam
- * admin only: update a particular team title/ description/ team_lead by id
- * PATCH /api/v1/teams/:id
- */
-const updateTeam = catchAsync(
-    /** @type {RequestHandler} */
-    async (req, res, next) => {
-        // If request body is invalid
-        if (!req.body) return next(new AppError(400, 'Not valid request body'));
+// /**
+//  * UpdateTeam
+//  * admin only: update a particular team title/ description/ team_lead by id
+//  * PATCH /api/v1/teams/:id
+//  */
+// const updateTeam = catchAsync(
+//     /** @type {RequestHandler} */
+//     async (req, res, next) => {
+//         // If request body is invalid
+//         if (!req.body) return next(new AppError(400, 'Not valid request body'));
 
-        const filtered = filterBody(req.body, 'title', 'description', 'teamLead');
+//         const filtered = filterBody(req.body, 'title', 'description', 'teamLead');
 
-        if (Object.keys(filtered).length === 0) return next(new AppError(400, "No valid fields to update"));
+//         if (Object.keys(filtered).length === 0) return next(new AppError(400, "No valid fields to update"));
 
-        if (filtered.teamLead) {
-            if ((await Users.findById(teamLead).select('role')).role !== 'team_lead') {
-                return next(new AppError(400, 'Only users with team_lead role can be assigned as team lead'))
-            }
-        }
+//         if (filtered.teamLead) {
+//             if ((await Users.findById(teamLead).select('role')).role !== 'team_lead') {
+//                 return next(new AppError(400, 'Only users with team_lead role can be assigned as team lead'))
+//             }
+//         }
 
-        // Find team
-        const team = await Teams.findById(req.params.id);
-        if (!team) return next(new AppError(404, 'Team is not found'));
-        if (!team.isActive) return next(new AppError(404, 'Team is not active'));
+//         // Find team
+//         const team = await Teams.findById(req.params.id);
+//         if (!team) return next(new AppError(404, 'Team is not found'));
+//         if (!team.isActive) return next(new AppError(404, 'Team is not active'));
 
-        const updatedTeam = await Teams.findByIdAndUpdate(
-            req.params.id,
-            filtered,
-            { returnDocument: 'after', runValidators: true }
-        ).populate([
-            { path: 'teamLead', select: 'name email' },
-            { path: 'members', select: 'name email' }
-        ])
+//         const updatedTeam = await Teams.findByIdAndUpdate(
+//             req.params.id,
+//             filtered,
+//             { returnDocument: 'after', runValidators: true }
+//         ).populate([
+//             { path: 'teamLead', select: 'name email' },
+//             { path: 'members', select: 'name email' }
+//         ])
 
-        res.status(200).json({
-            success: true,
-            data: updatedTeam
-        })
-    }
-)
+//         res.status(200).json({
+//             success: true,
+//             data: updatedTeam
+//         })
+//     }
+// )
 
-/**
- * DeleteTeam
- * admin only: get a particular team by id
- * DELETE /api/v1/teams/:id
- */
-const deleteTeam = catchAsync(
-    /** @type {RequestHandler} */
-    async (req, res, next) => {
-        // Find Team
-        const team = await Teams.findById(req.params.id)
-        if (!team) return next(new AppError(404, 'Team is not found'));
+// /**
+//  * DeleteTeam
+//  * admin only: get a particular team by id
+//  * DELETE /api/v1/teams/:id
+//  */
+// const deleteTeam = catchAsync(
+//     /** @type {RequestHandler} */
+//     async (req, res, next) => {
+//         // Find Team
+//         const team = await Teams.findById(req.params.id)
+//         if (!team) return next(new AppError(404, 'Team is not found'));
 
-        if (!team.isActive) return next(new AppError(400, 'Team is already deactive'));
+//         if (!team.isActive) return next(new AppError(400, 'Team is already deactive'));
 
-        team.isActive = false;
-        await team.save();
+//         team.isActive = false;
+//         await team.save();
 
-        res.status(204).send();
-    }
-)
+//         res.status(204).send();
+//     }
+// )
 
-/**
- * teamReactivate
- * admin only: get a particular team by id
- * DELETE /api/v1/teams/:id
- */
-const teamReactivate = catchAsync(
-    /** @type {RequestHandler} */
-    async (req, res, next) => {
-        // Find Team
-        const team = await Teams.findById(req.params.id);
-        if (!team) return next(new AppError(404, 'Team is not found'));
+// /**
+//  * teamReactivate
+//  * admin only: get a particular team by id
+//  * DELETE /api/v1/teams/:id
+//  */
+// const teamReactivate = catchAsync(
+//     /** @type {RequestHandler} */
+//     async (req, res, next) => {
+//         // Find Team
+//         const team = await Teams.findById(req.params.id);
+//         if (!team) return next(new AppError(404, 'Team is not found'));
 
-        if (team.isActive) return next(new AppError(400, 'Team is already active'));
+//         if (team.isActive) return next(new AppError(400, 'Team is already active'));
 
-        team.isActive = true;
-        await team.save();
+//         team.isActive = true;
+//         await team.save();
 
-        res.status(200).json({
-            success: true,
-            data: team
-        })
-    }
-)
+//         res.status(200).json({
+//             success: true,
+//             data: team
+//         })
+//     }
+// )
 
 module.exports = {
     createProject,
-    getAllProjects,
-    getProject,
-    updateTeam,
-    deleteTeam,
-    teamReactivate
+    // getAllProjects,
+    // getProject,
+    // updateTeam,
+    // deleteTeam,
+    // teamReactivate
 };
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -1,27 +1,27 @@
 const {
     createProject,
-    getAllProjects,
-    getProject,
-    updateTeam,
-    deleteTeam,
-    teamReactivate
+    // getAllProjects,
+    // getProject,
+    // updateTeam,
+    // deleteTeam,
+    // teamReactivate
 } = require("../controllers/project.controller.js");
 
 const { protect, restrictTo } = require("../middlewares/auth.middleware.js");
 
 const router = require("express").Router();
 
-// // ----------------------
-// // Project Routes
-// // ----------------------
+// ----------------------
+// Project Routes
+// ----------------------
 
-// // Admin, team_lead: Create new project
-// // Admin: get all projects
-// // POST /api/v1/projects      → create team
-// // GET  /api/v1/projects     → get all projects
-// router.route("/")
-//     .post(restrictTo('admin', 'team_lead'), createProject)
-//     .get(restrictTo('admin'), getAllProjects)
+// Admin, team_lead: Create new project
+// Admin: get all projects
+// POST /api/v1/projects      → create team
+// GET  /api/v1/projects     → get all projects
+router.route("/")
+    .post(restrictTo('admin', 'team_lead'), createProject)
+    // .get(restrictTo('admin'), getAllProjects)
 
 // // admin/ team_lead/ member: get a particular project by id;
 // // only admin: update or delete a team by id
