6b830c6c90e8b1e7ba66a69bba08ddea9053d367
Muhammad Apu <apu16-334@diu.edu.bd>
2026-06-03 22:49:27 +0600

    Updated getIssue controller business logics

--- src/controllers/issue.controller.js
+++ src/controllers/issue.controller.js
@@ -120,15 +120,9 @@ const getIssue = catchAsync(
 
         if (!issue) return next(new AppError(404, 'issue is not found'));
 
-        // then if issue is cancelled
-        if (issue.status === 'cancelled') {
-            const errArray = req.user.role === 'member'
-                ? [404, 'issue is not found']
-                : [400, `issue is ${issue.status}`];
-
-            return next(new AppError(errArray[0], errArray[1]));
-        }
-
+        // then if issue is cancelled and logged user is member
+        if (issue.status === 'cancelled' && req.user.role === 'member') return next(new AppError(404, 'issue is not found'));
+        
         // if logged user is not team lead of this issue project team
         if (req.user.role === 'team_lead' && issue.project.team.teamLead.toString() !== req.user.id) {
             return next(new AppError(403, 'Team lead can get his or her teams projects issues only'));
@@ -270,7 +264,7 @@ const deleteIssue = catchAsync(
                 {
                     path: 'project', select: 'title team', populate: {
                         path: 'team',
-                        select: 'title teamLead members'
+                        select: 'title teamLead'
                     }
                 }
             );
